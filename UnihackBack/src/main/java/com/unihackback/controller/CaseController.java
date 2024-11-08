a84460831389092e1beec22afb20abdba2b7c399
Daniel <turutadaniel20@gmail.com>
2024-11-08 19:10:36 +0200

    controller/service

---
+++ UnihackBack/src/main/java/com/unihackback/controller/CaseController.java
@@ -0,0 +1,28 @@
+package com.unihackback.controller;
+
+import com.unihackback.entity.Case;
+import com.unihackback.service.CaseService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller(value = "/case")
+public class CaseController {
+
+    private final CaseService caseService;
+
+    public CaseController(CaseService caseService) {
+        this.caseService = caseService;
+    }
+
+    @GetMapping(value = "/case/{caseId}", produces = "application/json")
+    public Case getCaseById(String caseId) {
+        return caseService.getCaseById(caseId);
+    }
+
+    @GetMapping(value = "/cases-by-doctor/{doctorId}", produces = "application/json")
+    public Case getCasesByDoctor(String doctorId) {
+        return caseService.getCasesByDoctor(doctorId);
+    }
+
+
+}
---
+++ UnihackBack/src/main/java/com/unihackback/service/ReportService.java
@@ -0,0 +1,29 @@
+package com.unihackback.service;
+
+import com.unihackback.repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.unihackback.entity.Report;
+
+import javax.imageio.spi.ServiceRegistry;
+import java.util.List;
+
+@Service
+public class ReportService {
+
+    ReportRepository reportRepository;
+
+    @Autowired
+    public ReportService(ReportRepository reportRepository) {
+        this.reportRepository = reportRepository;
+    }
+
+    public Report getReportById(String reportId) {
+        return reportRepository.findById(reportId).orElse(null);
+    }
+
+    public List<Report> getAllReports() {
+        return reportRepository.findAll();
+    }
+
+}
