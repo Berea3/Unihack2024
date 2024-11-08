9d088d52fcc278d28e2cd2a117dd9090a420a8f9
Berendea Andrei <berendeaandrei03@gmail.com>
2024-11-08 21:02:44 +0200

    value and talent

--- UnihackFront/src/app/app.routes.ts
+++ UnihackFront/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import {AuthenticationComponent} from './components/authentication/authentication.component';
 
 export const routes: Routes = [
-    { path: '', component: AuthenticationComponent },
+    { path: 'authentication', component: AuthenticationComponent },
 ];
