d34c48c605b1772c19076ed76043b504256b2580
Daniel <turutadaniel20@gmail.com>
2024-11-09 12:58:07 +0200

    working shit

--- UnihackBack/src/main/java/com/unihackback/controller/UserController.java
+++ UnihackBack/src/main/java/com/unihackback/controller/UserController.java
@@ -1,9 +1,8 @@
 package com.unihackback.controller;
 
 import com.unihackback.security.entity.User;
-import com.unihackback.service.UserService;
+import com.unihackback.security.entity.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +13,11 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-    UserService userService;
-
     @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    UserRepository userRepository;
 
     @GetMapping("/get-all-doctors")
     public List<User> getAllDoctors() {
-        return userService.getAllDoctors();
+        return userRepository.findAllDoctors();
     }
 }
--- UnihackBack/src/main/java/com/unihackback/security/entity/UserRepository.java
+++ UnihackBack/src/main/java/com/unihackback/security/entity/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -11,5 +12,8 @@ public interface UserRepository extends JpaRepository<User,String> {
 
     @Query(value="select user from User user where user.email = ?1")
     User findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.roles = 'DOCTOR'")
+    List<User> findAllDoctors();
 
 }
